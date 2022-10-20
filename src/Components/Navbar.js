7148c03d06b7936b345e385b200bd53cd984fe0f
Aaditya <aadityanitin@gmail.com>
2022-10-20 23:52:09 +0530

    :sparkles:feat:Added navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { AppBar, Toolbar, Typography, styled } from '@mui/material'
+
+const Header = styled(AppBar)`
+background: #111111
+`
+const Tabs = styled(Typography)`
+font-size:20px;
+margin-right:20px;
+`
+
+const Navbar = () => {
+  return (
+    <div>
+      <Header>
+        <Toolbar>
+         <Tabs>A CRUD App</Tabs>
+         <Tabs>All Users</Tabs>   
+         <Tabs>Add Users</Tabs>   
+        </Toolbar>
+      </Header>
+    </div>
+  )
+}
+
+export default Navbar
