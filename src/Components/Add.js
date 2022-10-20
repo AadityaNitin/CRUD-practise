f87f2bbed67ac5021c7b775f38cb02262cad2e97
Aaditya <aadityanitin@gmail.com>
2022-10-21 01:34:35 +0530

    :sparkles:feat:Added API

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FormControl, FormGroup, InputLabel, Input, Typography, Button, styled  } from '@mui/material'
+import { addUser } from '../Service/api'
 
 const Container = styled(FormGroup)`
 width:50%;
@@ -9,29 +10,48 @@ margin-top:1em;
 }
 `
 
+const initialValues = {
+    name:'',
+    username:'',
+    email:'',
+    phone:''
+}
+
 const Add = () => {
+
+    const [user, setUser] = useState(initialValues)
+
+    const onValueChange = (e) => {
+        setUser( { ...user, [e.target.name]: e.target.value} )
+        console.log(user);
+    }
+
+    const addUserDetails = async () => {
+        await addUser(user);
+    }
+
   return (
     <div>
       <Container>
         <Typography variant='h4'>Add User</Typography>
         <FormControl>
             <InputLabel>Name</InputLabel>
-            <Input />
+            <Input onChange={ (e) => onValueChange(e)} name="name"/>
         </FormControl>
         <FormControl>
             <InputLabel>Username</InputLabel>
-            <Input />
+            <Input onChange={ (e) => onValueChange(e)} name="username"/>
         </FormControl>
         <FormControl>
             <InputLabel>Email</InputLabel>
-            <Input />
+            <Input onChange={ (e) => onValueChange(e)} name="email"/>
         </FormControl>
         <FormControl>
             <InputLabel>Phone</InputLabel>
-            <Input />
+            <Input onChange={ (e) => onValueChange(e)} name="phome"/>
         </FormControl>
         <FormControl>
-            <Button variant='contained'>Add User</Button>
+            <Button onClick={() => addUserDetails()} variant='contained'>Add User</Button>
         </FormControl>
       </Container>
     </div>
