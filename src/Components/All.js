ae7eb2f8ddefa6250a1b4c3a322cf4e691ba13ff
Aaditya <aadityanitin@gmail.com>
2022-10-21 14:48:06 +0530

    :sparkles:feat:Added all users page

--- src/Components/All.js
+++ src/Components/All.js
@@ -1,9 +1,73 @@
+import { useEffect, useState } from 'react';
+import { Table, TableHead, TableBody, TableRow, TableCell, styled, Button } from '@mui/material'
 import React from 'react'
+import { getUsers } from '../Service/api';
+import { Link } from 'react-router-dom';
+
+const StyledTable = styled(Table)`
+width:90%;
+margin: 4em auto 0 auto;
+`;
+
+const Thead = styled(TableRow)`
+background: #000;
+& > th {
+  color: #fff;
+  font-size:20px;
+}
+`
+
+const TBody = styled(TableRow)`
+& > td {
+  font-size:20px;
+}
+`
 
 const All = () => {
+  
+  useEffect(() => {
+    getUsersDetails();
+  }, []);
+
+  const [users, setUsers] = useState([]);
+
+
+  const getUsersDetails = async () => {
+      let response = await getUsers();
+      console.log(response);
+      setUsers(response.data);
+  }
   return (
     <div>
-      <p>Hello All</p>
+      <StyledTable>
+        <TableHead>
+          <Thead>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Username</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Phone</TableCell>
+            <TableCell></TableCell>
+          </Thead>
+        </TableHead>
+        <TableBody>
+          {
+            users.map(user => (
+              <TBody>
+                <TableCell>{user.id}</TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.username}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{user.phone}</TableCell>
+                <TableCell>
+                  <Button variant='contained' color='secondary' style={{marginRight:10}} component={Link} to={`/edit/${user.id}`}>Edit</Button>
+                  <Button variant='contained' color='error' >Delete</Button>
+                </TableCell>
+              </TBody>
+            ))
+          } 
+        </TableBody>
+      </StyledTable>
     </div>
   )
 }
